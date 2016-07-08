b0e6fa4a2a9c2a43cc07e985e100561225dc41ab
PanzerKunst99 <loic.tabaglio@gmail.com>
2016-07-08 15:21:58 +0200

    add
    verif du media

--- app/Controller/BlogController.php
+++ app/Controller/BlogController.php
@@ -21,12 +21,12 @@ class BlogController extends Controller
 
         //ajouter un article
         
-        $data = array(
-            
-        )
-        public function insert(array $data, $stripTags = true)
+
+        public function add()
         {
+            var_dump($_FILES); //etat des lieux mettre en com a la fin ou supprimer la ligne
         // Autorisation
+       
         
         
             // Je verifie si j'ai une soumission de formulaire
@@ -43,34 +43,82 @@ class BlogController extends Controller
                 if(empty($_POST['content'])) {
                     $errors['content']['empty'] = true;
                 }
+                if(empty($_FILES['picture'])) {
+                    $errors['content']['empty'] = true;
+                }
 
                 // Ajout en DB
                 if(count($errors) === 0) {
                     // Si j'ai pas d'erreur
-                    $name = htmlentities($_POST['name']);
-                    $importance = $_POST['importance'];
-                    $dateEnd = $_POST['date-end'];
+                    $name = $_POST['title'];
+                    $dateCreated = $_POST['dateCreated'];
+                    $content = $_POST['content'];
+                    $picture = $_FILES['picture']
 
                     $manager = new \Manager\TaskManager();
                     $manager->insert([
-                        'name' => $name,
-                        'degree_of_importance' => $importance,
-                        'date_end' => $dateEnd,
+                        'title' => $title,
+                        'dateCreated' => $dateCreated,
+                        'content' => $content,
+                        'picture' => $picture,
                     ]);
 
-                    if(isset($_POST['add-task'])) {
+                    if(isset($_POST['addArticle'])) {
                         // Ajout et redirection
-                        $this->redirectToRoute('task_list');
+                        $this->redirectToRoute('home');
                     } else { // Facultatif
                         // Si j'ai clique sur 'rester'
-                        $this->redirectToRoute('task_add');
+                        $this->redirectToRoute('home');
                     }
+                    
+                    // traitement du medias
+                    // Vérifier si le téléchargement du fichier n'a pas été interrompu
+                    if ($_FILES['picture']['error'] != UPLOAD_ERR_OK) {
+                        echo 'Erreur lors du téléchargement.';
+                    } else {
+                        // Objet FileInfo
+                        $finfo = new finfo(FILEINFO_MIME_TYPE);
+
+                        // Récupération du Mime
+                        $mimeType = $finfo->file($_FILES['picture']['tmp_name']);
+
+                        $extFoundInArray = array_search(
+                            $mimeType, array(
+                                'jpg' => 'image/jpeg',
+                                'png' => 'image/png',
+                                'gif' => 'image/gif',
+                            )
+                        );
+                        if ($extFoundInArray === false) {
+                            echo 'Le fichier n\'est pas une image';
+                        } else {
+                            // Si on a bien recu l'image, mais qu'elle fait moins de 50x50 px
+                            $size = getimagesize($_FILES['picture']['tmp_name']);
+                            // $size[0] contient la largeur
+                            // $size[1] contient la hauteur
+                            if ($size[0] < 50 || $size[1] < 50) {
+                                $errors['imageTooSmall'] = true;
+                            } else {
+
+                                // Renommer nom du fichier
+                                $fileName = sha1_file($_FILES['picture']['tmp_name']) . time() . '.' . $extFoundInArray;
+                                $path = './uploads/' . $fileName;
+                                $moved = move_uploaded_file($_FILES['picture']['tmp_name'], $path);
+                                if (!$moved) {
+                                    echo 'Erreur lors de l\'enregistrement';
+                                } else {
+                                    // On a bien uploadé le fichier
+                                    addPic($pdo, $_POST['name'], $fileName, $_POST['legend']);
+                                }
+                            } // End Move File
+                        } // End File Is Image
+                    } // End Upload Success
                 }
                 // Si j'ai une erreur
                 // J'affiche le template, avec un tableau d'erreurs
-                $this->show('task/add', $errors);
+                $this->show('blog/add', $errors);
             }
         
-        $this->show('task/add');
+        $this->show('blog/add');
         }
 }
