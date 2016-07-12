53617f366fddcc7d853fe1e241452d45fae3dd6f
Marielle <antoine.marielle@orange.fr>
2016-07-12 16:38:23 +0200

    commit12

--- app/routes.php
+++ app/routes.php
@@ -3,13 +3,21 @@
 	$w_routes = array(
 
 		['GET', '/blog', 'Blog#home', 'home'],
+
+		// Affichage en liste ou en grille
         ['GET', '/blog/liste', 'Blog#liste', 'liste'],
         ['GET', '/blog/grid', 'Blog#grid', 'grid'],
+
+        // Gestion des articles
         ['GET', '/blog/article/[:id]', 'Blog#article', 'article'],
         ['GET|POST', '/blog/add', 'Blog#add', 'add'],
 		['GET|POST', '/blog/delete/[:id]', 'Blog#delete', 'delete'],
+
+		// Recherches spécifiques
 		['GET|POST', '/blog/category/[:id]', 'Blog#category', 'category'],
-		['GET|POST', '/blog/login', 'User#login', 'login'],
 		['GET', '/blog/search', 'Blog#search', 'search'],
+
+		// Connexion
+		['GET|POST', '/blog/login', 'User#login', 'login'],
 
 	);
