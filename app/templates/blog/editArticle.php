dfc7577cdaeaf52c9eefbb2876711e6e9264488d
PanzerKunst99 <loic.tabaglio@gmail.com>
2016-07-19 10:54:00 +0200

    edit

--- app/templates/blog/editArticle.php
+++ app/templates/blog/editArticle.php
@@ -5,38 +5,41 @@
 <form action="#" method="POST" accept-charset="utf-8" enctype="multipart/form-data">
 
 	<label>
-		<input type="text" name="newTitle" value="<?= $articles['title'] ?>">
-		<?php if (isset($errors['newTitle']['empty'])): ?>
+		<input type="text" name="title" value="<?= $article['title'] ?>">
+		<?php if (isset($errors['title']['empty'])): ?>
 		Titre non renseigné
 		<?php endif; ?>
 	</label>
     
     	<label>
-		<input type="text" name="newAuthor" value="<?= $articles['author'] ?>">
-		<?php if (isset($errors['newAuthor']['empty'])): ?>
+		<input type="text" name="author" value="<?= $article['author'] ?>">
+		<?php if (isset($errors['author']['empty'])): ?>
 		Auteur non renseigné
 		<?php endif; ?>
 	</label>
     
     <label>
-		<input type="text" name="newDateCreated" value="<?= date('d/m/Y', strtotime($article['dateCreated'])) ?>">
-		<?php if (isset($errors['newDateCreated']['empty'])): ?>
+		<input type="text" name="dateCreated" value="<?= date('d/m/Y', strtotime($article['dateCreated'])) ?>">
+		<?php if (isset($errors['dateCreated']['empty'])): ?>
 		remplis la nouvelle date
 		<?php endif; ?>
 	</label>
     
 	<label>
-        <textarea name="newContent" class="admin" ><?= $articles['content'] ?></textarea>
-        <?php if (isset($errors['newContent']['empty'])): ?>
+        <textarea name="content" class="admin" ><?= $article['content'] ?></textarea>
+        <?php if (isset($errors['content']['empty'])): ?>
 		Contenu inexistant
 		<?php endif; ?>
 	</label>
     
-    <label>
-        <input type="file" name="newPicture" src="/uploads/<?= $article['picture'] ?>">
-        <?php if (isset($errors['newPicture']['empty'])): ?>
+        <label>
+            <figure>
+                <img src="/uploads/<?php echo $article['picture'] ?>" alt="" />
+            </figure>
+            <input type="file" name="picture" src="/uploads/<?= $article['picture'] ?>">
+            <?php if (isset($errors['picture']['empty'])): ?>
 		Bannière inexistante
-		<?php endif; ?>
+            <?php endif; ?>
 	</label>
             
     <button type="submit" name="editArticle">validé votre modification</button>
