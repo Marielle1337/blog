5418247421dbb3b3a0a39e7273986237ae9fdff8
Marielle <antoine.marielle@orange.fr>
2016-07-22 13:43:15 +0200

    truc

--- app/Controller/MailController.php
+++ app/Controller/MailController.php
@@ -50,7 +50,7 @@ class MailController extends Controller
             // Si aucune erreur
             if(count($errors) == 0) {
                 $title = $_POST['title'];
-                $sendDate = $_POST['sendDate'];
+                $sendDate = date("Y-m-d", strtotime($_POST['sendDate']);
                 $content = $_POST['content'];
 
                 $managerNewsletters = new \Manager\MailManager();
